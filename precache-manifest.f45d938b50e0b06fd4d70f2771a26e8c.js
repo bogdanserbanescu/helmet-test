e65a4e5ccd49d81cb2c5ede8d1e04294495efe0b
Bogdan Serbanescu <bogdan.serbanescu@vgw.co>
2019-07-23 13:59:36 +0300

    Updates

---
+++ precache-manifest.f45d938b50e0b06fd4d70f2771a26e8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b84aca9b5e3dfea4ec2aa918160009",
+    "url": "/helmet-test/index.html"
+  },
+  {
+    "revision": "bb98649420b7cd2661d5",
+    "url": "/helmet-test/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0ea8026a90fdd0d7ec2d",
+    "url": "/helmet-test/static/js/2.db9edd5f.chunk.js"
+  },
+  {
+    "revision": "bb98649420b7cd2661d5",
+    "url": "/helmet-test/static/js/main.b95060e1.chunk.js"
+  },
+  {
+    "revision": "70d0ba53e4829557ce36",
+    "url": "/helmet-test/static/js/runtime~main.0fd91361.js"
+  }
+]);
