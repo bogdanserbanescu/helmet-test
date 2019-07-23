2f3e4159aaf09130a288077626834d3d651c0747
Bogdan Serbanescu <bogdan.serbanescu@vgw.co>
2019-07-23 16:14:04 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Helmet} from "react-helmet";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function Index() {
@@ -10,7 +11,16 @@ function About() {
 }
 
 function Users() {
-  return <h2>Users</h2>;
+  return <Helmet>
+          <meta property="fb:app_id" content="277341966096239" />
+          <html prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# game: http://ogp.me/ns/game#"></html>
+          <meta property="og:type" content="game.achievement" />
+          <meta property="og:title" content="Free Spins!" />
+          <meta property="og:description" content="I just won Free Spins! Come join the adventure in LuckyLand Slots, where you can win real cash prizes!" />
+          <meta property="og:image" content="https://play.luckylandslots.com/loader/facebook/assets/freespins.png" />
+          <meta property="og:image:width" content="480" />
+          <meta property="og:image:height" content="248" />
+        </Helmet>;
 }
 
 function AppRouter() {
